119a99d50417595b32e3fe9189fcc926477adcf6
mihirmithani <mihirmithani123@gmail.com>
2026-08-25 16:37:21 +0530

    Experiment 4

--- Experiments/Experiment4_MultimediaPlayer/src/main/java/com/example/multimedia/MainActivity.kt
+++ Experiments/Experiment4_MultimediaPlayer/src/main/java/com/example/multimedia/MainActivity.kt
@@ -21,6 +21,8 @@ import androidx.media3.common.MediaItem
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.ui.PlayerView
 
+import androidx.compose.ui.tooling.preview.Preview
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,6 +36,14 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MultimediaAppPreview() {
+    MultimediaTheme {
+        MultimediaApp()
     }
 }
 
